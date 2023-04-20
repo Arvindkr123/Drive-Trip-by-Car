a38def958cdc98c877110c440f25da79202ccdbf
Arvind <thakurarvindkr10@gmail.com>
2023-04-20 21:00:58 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/hero/Hero';
+import Find from './components/find/Find';
+import Driver from './components/driver/Driver';
+import Luxury from './components/luxury/Luxury';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
     <div>
-      <h1>Drive travel by Car</h1>
+      <Navbar />
+      <Hero />
+      <Find />
+      <Driver />
+      <Luxury />
+      <Footer/>
     </div>
   )
 }
