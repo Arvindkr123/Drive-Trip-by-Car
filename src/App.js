3bd7fd5cd564a6387abb0c975593fcdfee5b2986
Arvind <thakurarvindkr10@gmail.com>
2023-04-22 06:39:46 +0530

    deleteing old files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/hero/Hero';
-import Find from './components/find/Find';
-import Driver from './components/driver/Driver';
-import Luxury from './components/luxury/Luxury';
-import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <Find />
-      <Driver />
-      <Luxury />
-      <Footer/>
+      <h1>Hello World</h1>
     </div>
   )
 }
